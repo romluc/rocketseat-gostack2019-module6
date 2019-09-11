844f8d8aba28c60e55fb47979a85f15a604fedc3
romluc <romulofilhofilho@gmail.com>
2019-09-11 20:16:48 -0300

    Initial Commit

---
+++ src/pages/Main/styles.js
@@ -0,0 +1,115 @@
+import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
+
+export const Container = styled.View`
+  flex: 1;
+  padding: 30px;
+`;
+
+export const Form = styled.View`
+  flex-direction: row;
+  padding-bottom: 20px;
+  border-bottom-width: 1px;
+  border-color: #eee;
+`;
+
+export const Input = styled.TextInput.attrs({
+  placeholderTextColor: '#999',
+})`
+  flex: 1;
+  height: 40px;
+  background: #eee;
+  border-radius: 4px;
+  padding: 0 15px;
+  border: 1px solid #eee;
+`;
+
+export const SubmitButton = styled(RectButton)`
+  justify-content: center;
+  align-items: center;
+  background: #008b8b;
+  border-radius: 4px;
+  margin-left: 10px;
+  padding: 0 12px;
+  opacity: ${props => (props.loading ? 0.7 : 1)};
+`;
+
+export const List = styled.FlatList.attrs({
+  showsVerticalScrollIndicator: false,
+})`
+  margin-top: 20px;
+`;
+
+export const User = styled.View`
+  align-items: center;
+  margin: 0 20px 30px;
+`;
+
+export const Avatar = styled.Image`
+  width: 80px;
+  height: 80px;
+  border-radius: 40px;
+  background: #eee;
+`;
+
+export const Name = styled.Text`
+  font-size: 14px;
+  color: #333;
+  font-weight: bold;
+  margin-top: 4px;
+  text-align: center;
+`;
+
+export const Bio = styled.Text.attrs({
+  numberOfLines: 2,
+})`
+  font-size: 13px;
+  line-height: 18px;
+  color: #999;
+  margin-top: 5px;
+  text-align: center;
+`;
+
+export const ProfileButton = styled(RectButton)`
+  margin-top: 10px;
+  align-self: stretch;
+  border-radius: 4px;
+  background: #008b8b;
+  justify-content: center;
+  align-items: center;
+  height: 36px;
+`;
+
+export const ProfileButtonText = styled.Text`
+  font-size: 14px;
+  font-weight: bold;
+  color: #fff;
+  text-transform: uppercase;
+`;
+
+export const ButtonContainer = styled.View`
+  flex-direction: row;
+  justify-content: flex-end;
+  padding-top: 10px;
+  border-top-width: 1px;
+  border-color: #eee;
+`;
+
+export const ClearAsyncStorageButton = styled(RectButton)`
+  margin-top: 10px;
+  border-radius: 4px;
+  background: #478bbf;
+  justify-content: center;
+  align-items: center;
+  height: 36px;
+  border-top-width: 1px;
+  border-color: #eee;
+`;
+
+export const ClearAsyncStorageButtonText = styled.Text`
+  font-size: 14px;
+  font-weight: bold;
+  padding: 0 5px;
+  color: #fff;
+  text-transform: uppercase;
+`;
